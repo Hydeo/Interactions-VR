30b0795c00f976c4da690c84edb2374dafeb0214
Hydeo <p.garciapelayo@gmail.com>
2020-11-29 13:31:59 +0000

    Teleport locomotion XR basic components

---
+++ Assets/HideWhenHit.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideWhenHit : MonoBehaviour
+{
+    public float hidingTime = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Coliding");
+        Hide();
+        Invoke("Show", hidingTime);
+    }
+
+    private void Show()
+    {
+        GetComponent<MeshRenderer>().enabled = true;
+    }
+
+    private void Hide()
+    {
+        GetComponent<MeshRenderer>().enabled = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
