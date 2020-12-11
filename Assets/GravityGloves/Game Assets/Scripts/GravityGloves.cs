b403c9b3ad22b5b33f05f015f8ee096dbe111d25
Hydeo <p.garciapelayo@gmail.com>
2020-12-11 12:51:15 +0000

    Gravity Gloves targeting improvement and discrimination

--- Assets/GravityGloves/Game Assets/Scripts/GravityGloves.cs
+++ Assets/GravityGloves/Game Assets/Scripts/GravityGloves.cs
@@ -43,6 +43,8 @@ public class GravityGloves : MonoBehaviour
     [SerializeField]
     List<GameObject> currentObjectsReachable;
     GameObject currentObjectFocused;
+    GameObject currentObjectActive;
+
 
     [SerializeField]
     GameObject collisionVolume;
@@ -52,7 +54,7 @@ public class GravityGloves : MonoBehaviour
     //Tracked Controller devices (position, velocity, ...)
     private Transform particulesInstance;
 
-    public String ALLOW_MANIPULATION_TAG = "ALLOW_MANIPULATION";
+    public String ALLOW_MANIPULATION_LAYER = "GrabInteractible";
     public Color reachableObjectOutlineColor;
     public Color focusObjectOutlineColor;
     public bool isGloveActivated = false;
@@ -66,7 +68,7 @@ public class GravityGloves : MonoBehaviour
 
     public void OnFocusTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag(ALLOW_MANIPULATION_TAG) && !isGloveActivated)
+        if (IsOnInteractableLayer(other) && !isGloveActivated)
         {
             UpdateObjectOutlineIfManipulable(focusObjectOutlineColor, other);
             currentObjectFocused = other.gameObject;
@@ -74,7 +76,7 @@ public class GravityGloves : MonoBehaviour
     }
     public void OnFocusTriggerExit(Collider other)
     {
-        if (other.gameObject.CompareTag(ALLOW_MANIPULATION_TAG) && !isGloveActivated)
+        if (IsOnInteractableLayer(other) && !isGloveActivated)
         {
             UpdateObjectOutlineIfManipulable(reachableObjectOutlineColor, other);
             currentObjectFocused = null;
@@ -83,7 +85,7 @@ public class GravityGloves : MonoBehaviour
 
     public void OnReachableTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag(ALLOW_MANIPULATION_TAG))
+        if (IsOnInteractableLayer(other))
         {
             UpdateObjectOutlineIfManipulable(reachableObjectOutlineColor, other);
             currentObjectsReachable.Add(other.gameObject);
@@ -92,7 +94,7 @@ public class GravityGloves : MonoBehaviour
 
     public void OnReachableTriggerExit(Collider other)
     {
-        if (other.gameObject.CompareTag(ALLOW_MANIPULATION_TAG))
+        if (IsOnInteractableLayer(other))
         {
             Outline objectOutline = other.gameObject.GetComponent<Outline>();
             if (objectOutline)
@@ -103,6 +105,10 @@ public class GravityGloves : MonoBehaviour
         }
     }
 
+    private bool IsOnInteractableLayer(Collider c)
+    {
+        return c.gameObject.layer == LayerMask.NameToLayer(ALLOW_MANIPULATION_LAYER);
+    }
     private void UpdateObjectOutlineIfManipulable(Color c, Collider other)
     {
 
@@ -140,12 +146,6 @@ public class GravityGloves : MonoBehaviour
         return velocity;
     }
 
-    private Vector3 GetDevicePosition(InputDevice d)
-    {
-        d.TryGetFeatureValue(CommonUsages.devicePosition, out Vector3 position);
-        return position;
-    }
-
     private bool IsGripActivated(InputDevice d)
     {
         d.TryGetFeatureValue(CommonUsages.gripButton, out bool gripValue);
@@ -161,7 +161,7 @@ public class GravityGloves : MonoBehaviour
     private void UpdateLineRenderer()
     {
         //If we aim at a valid target
-        if (currentObjectFocused != null)
+        if (currentObjectActive != null)
         {
             UpdateLineRendererPosition(transform.InverseTransformPoint(this.transform.position), transform.InverseTransformPoint(currentObjectFocused.transform.position));
             handLineRenderer.enabled = true;
@@ -170,14 +170,6 @@ public class GravityGloves : MonoBehaviour
         {
             handLineRenderer.enabled = false;
         }
-    }
-
-    private bool TargetUnfocused()
-    {
-        return
-            (particulesInstance != null && particulesInstance.gameObject != null) //If we have  particules showing
-            && (!IsGripActivated(handDevice) && currentObjectFocused != null) // The player is not gripping and we had a target
-            || (IsGripActivated(handDevice) && currentObjectFocused != null && currentObjectFocused != null);//If we grip, had a target but don't aim at anytarget anymore
     }
 
     // Update is called once per frame
@@ -201,10 +193,11 @@ public class GravityGloves : MonoBehaviour
             case Spell.ATTRACT:
                 {
                     
-                    if(!IsGripActivated(handDevice))
+                    if(!IsGripActivated(handDevice) && isGloveActivated)
                     {
                         isGloveActivated = false;
                     }
+
                     UpdateLineRenderer();
 
                     //If we trigger aiming at a valid target
@@ -214,24 +207,27 @@ public class GravityGloves : MonoBehaviour
                         particulesInstance = Instantiate(grabbityTracker, currentObjectFocused.transform.position, Quaternion.Euler(0.0f, 0.0f, 0.0f));
                         particulesInstance.SetParent(currentObjectFocused.transform);
                         isGloveActivated = true;
+                        currentObjectActive = currentObjectFocused;
                     }
                     else if (!isGloveActivated && particulesInstance != null)
                     {
                         Destroy(particulesInstance.gameObject);
                         particulesInstance = null;
+                        currentObjectActive = null;
                     }
 
                     //Check for flick of hand
-                    if (currentObjectFocused != null && particulesInstance!= null && Vector3.Dot(GetDeviceVelocity(handDevice), (xr_rig.cameraGameObject.transform.forward - xr_rig.cameraGameObject.transform.up)) < -attractionSpellSensitivity)
+                    if (currentObjectActive != null && particulesInstance!= null && Vector3.Dot(GetDeviceVelocity(handDevice), (xr_rig.cameraGameObject.transform.forward - xr_rig.cameraGameObject.transform.up)) < -attractionSpellSensitivity)
                     {
                         //Calculate velocity and apply it to the target
-                        Vector3 calculated_velocity = ComplementarCalculations.CalculateParabola(currentObjectFocused.transform.position, handTransform.position);
+                        Vector3 calculated_velocity = ComplementarCalculations.CalculateParabola(currentObjectActive.transform.position, handTransform.position);
                         currentObjectFocused.GetComponent<Rigidbody>().velocity = calculated_velocity;
                         //Destroy particle system
                         if (particulesInstance != null)
                         {
                             Destroy(particulesInstance.gameObject);
                             particulesInstance = null;
+                            currentObjectActive = currentObjectFocused = null;
                         }
                     }
                     break;
